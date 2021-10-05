af749c6473b1f6da8269211e24f4f559a944fe49
Andreas <akbr18@student.aau.dk>
2021-10-05 19:15:56 +0200

    Changed the CreateSession test to be useful and represent domain logic.

--- internal/domain/identity/entity_session_test.go
+++ internal/domain/identity/entity_session_test.go
@@ -5,5 +5,18 @@ import (
 )
 
 func TestCreateSession(t *testing.T) {
-	CreateSession()
+	var session Session
+	var err error
+
+	session, err = CreateSession()
+
+	if err != nil {
+		t.Error("CreateSession failed with err:", err)
+	}
+	if session.id == "" {
+		t.Error("CreateSession created a session with an empty id")
+	}
+	if session.revoked {
+		t.Error("CreateSession created a session which was immediately revoked")
+	}
 }
