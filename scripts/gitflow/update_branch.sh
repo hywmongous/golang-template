6227d723c84ddeb0461d91e0ebccaccda7989b71
Brandhoej <akbr18@student.aau.dk>
2021-10-07 23:33:30 +0200

    Added docker stack deployment, changed to overlay network for distributed containers, added additional scripts and prepared for k8s with minikube deployment.

--- scripts/gitflow/update_branch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# If working on a "feature/x" the latest commit from
-# "develop" is merged into the "feature/x" you are working on.
-# However, if you are working on "develop" the latest commit
-# from "release/x" is merged into the "develop" branch.
-# Otherwise, if you are working on a "release/x" branch
-# then the latest commit from "develop" is merged into it.
--- scripts/gitflow/verify.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Verifies whether the giotflow strategy
-# is adhered to and reports any inconsistencies.
