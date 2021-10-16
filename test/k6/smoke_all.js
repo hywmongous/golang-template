919ecabd3b887415eeeba139ed4235d09016f224
Brandhoej <Brandhoej@users.noreply.github.com>
2021-10-16 23:59:17 +0000

    Prettified Code!

--- test/k6/smoke_all.js
+++ test/k6/smoke_all.js
@@ -1,21 +1,21 @@
-export { default as identity_login } from "./identity-login/smoke_test.js"
-export { default as identity_logout } from "./identity-logout/smoke_test.js"
+export { default as identity_login } from "./identity-login/smoke_test.js";
+export { default as identity_logout } from "./identity-logout/smoke_test.js";
 
 export const options = {
     minIterationDuration: `${minDuration}s`,
     scenarios: {
         identity_login: createSmokeScenario("identity_login"),
         identity_logout: createSmokeScenario("identity_logout"),
-    }
-}
+    },
+};
 
 function createSmokeScenario(func) {
     return {
         exec: func,
-        executor: 'constant-vus',
+        executor: "constant-vus",
         vus: 1,
-        duration: '10s',
+        duration: "10s",
         startTime: `0s`,
-        gracefulStop: '5s',
-    }
+        gracefulStop: "5s",
+    };
 }
