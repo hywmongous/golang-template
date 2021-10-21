0dc0cd501eee3c427c5bf174a2ceb0355a651e87
Brandhoej <Brandhoej@users.noreply.github.com>
2021-10-21 21:46:38 +0000

    Prettified Code!

--- test/k6/identity-logout/cases.js
+++ test/k6/identity-logout/cases.js
@@ -1,11 +1,7 @@
 import { check, sleep } from "k6";
 import http from "k6/http";
 import { successful_login } from "../identity-login/cases.js";
-import {
-    BASE_URL,
-    CSRF_HEADER_KEY,
-    initCases,
-} from "../index.js";
+import { BASE_URL, CSRF_HEADER_KEY, initCases } from "../index.js";
 
 export function successful_logout(
     username = "some1@email",
@@ -14,7 +10,7 @@ export function successful_logout(
     const login_response = successful_login(username, password);
 
     // Mimics a user session before logging out
-    sleep(0.1)
+    sleep(0.1);
 
     const logout_url = `${BASE_URL}/api/v1/authentication/logout`;
     const logout_body = null;
