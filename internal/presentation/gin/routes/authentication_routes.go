5ea13ef2cd7b114b7d2703438cc8a3e3251eeef9
Andreas <akbr18@student.aau.dk>
2021-10-22 18:45:46 +0200

    Fixed no handler for Auth PUT /verify http gin path.

--- internal/presentation/gin/routes/authentication_routes.go
+++ internal/presentation/gin/routes/authentication_routes.go
@@ -22,15 +22,15 @@ func CreateAuthenticationRoutes(
 
 func (routes AuthenticationRoutes) Setup() {
 	group := routes.engine.Group("/api/v1/authentication")
+	// PUT since we might come in a situation where we update
+	// the session. Eg. if the authentication for a given session
+	// has failed numerous times and we want to revoke the session.
+	// We dont use POST because we are not always creating a new resource.
+	// group.PUT("/verify")
 	// POST since we are creating a session upon logging in an account
 	group.POST("/login", routes.controller.Login)
 	// POST since it is not idempotent and we update the session
 	group.POST("/logout", routes.controller.Logout)
 	// POST since we are creating a new tokens and csrf
 	group.POST("/refresh", routes.controller.Refresh)
-	// PUT since we might come in a situation where we update
-	// the session. Eg. if the authentication for a given session
-	// has failed numerous times and we want to revoke the session.
-	// We dont use POST because we are not always creating a new resource.
-	group.PUT("/verify")
 }
