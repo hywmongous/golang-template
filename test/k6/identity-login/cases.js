7583ba6cac6b5e8b059ca5af1cadd08d0185ec99
Andreas <akbr18@student.aau.dk>
2021-10-24 18:06:09 +0200

    removed a console log, and added the identity register smoke test to the all collection.

--- test/k6/identity-login/cases.js
+++ test/k6/identity-login/cases.js
@@ -16,7 +16,7 @@ export function successful_login(
     username = getRandomEmail(),
     password = getRandomPassword()
 ) {
-    successful_registration(username, password);
+    successful_registration(username, password)
 
     // Mimics a user activating or doing something with the new identity
     sleep(0.1);
@@ -25,8 +25,6 @@ export function successful_login(
         `${username}:${password}`,
         "/api/v1/authentication/login"
     );
-
-    console.log(url);
 
     const login_response = http.post(url);
 
